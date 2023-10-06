fe830a6082e73042346e3630c38bc93031c9e3ee
Janzen Faidiban <janzenfaidiban@gmail.com>
2023-10-06 20:01:29 +0900

    updated: header, button

--- view-developers.php
+++ view-developers.php
@@ -16,12 +16,18 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
 </head>
-<body>
+<body class="bg-dark">
 
     <div class="container py-5">
         <div class="row">
             <div class="col-lg-10 mx-auto">
-                <div class="card">
+                <div class="card rounded-0">
+                    <div class="card-header">
+                        <h1>Data Developers at Nokensoft</h1>
+                        <a href="" class="btn btn-primary rounded-0">
+                            Add Developer
+                        </a>
+                    </div>
                     <div class="card-body">
 
 
